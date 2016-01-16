8946b114e58110cdcd0add10ebe5bb50a80f1421
Anirudh Ramanathan <anirudh4444@gmail.com>
2016-01-16 17:39:31 +0530

    Learned to use the indirect addressing mode using the A register

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -9,3 +9,80 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+
+@8192
+D=A
+
+@i
+M=D
+
+// above 2 steps set the value of i
+
+@SCREEN
+D=A
+
+@pos
+M=D
+
+// above 2 steps set pos
+
+@24576
+D=M
+
+// check that a key is pressed.
+@BLACKALL
+D;JEQ
+
+@WHITEALL
+D;JMP
+
+(END)
+@END
+0; JMP
+
+(BLACKALL)
+
+@i
+D=M
+
+@LOOP
+D;JEQ
+
+@pos
+A=M
+M=0
+
+@pos
+M=M+1
+
+@i
+M=M-1
+
+@BLACKALL
+0;JMP
+
+
+// PART2
+
+(WHITEALL)
+
+@i
+D=M
+
+@LOOP
+D;JEQ
+
+@pos
+A=M
+M=-1
+
+@pos
+M=M+1
+
+@i
+M=M-1
+
+@WHITEALL
+0;JMP
